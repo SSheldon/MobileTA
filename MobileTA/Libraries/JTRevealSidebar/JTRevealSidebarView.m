14ac66d99dca8a494327b8c11f4a98e880f1efe9
Scott Rice <meris608@gmail.com>
2013-02-11 17:27:36 -0600

    Added the JTRevealSidebar library

---
+++ MobileTA/Libraries/JTRevealSidebar/JTRevealSidebarView.m
@@ -0,0 +1,83 @@
+/*
+ * This file is part of the JTRevealSidebar package.
+ * (c) James Tang <mystcolor@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#import "JTRevealSidebarView.h"
+#import "JTNavigationView.h"
+
+@implementation JTRevealSidebarView
+
+@synthesize sidebarView, contentView;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        _state.isShowing = 0;
+    }
+    return self;
+}
+
+#pragma mark Properties
+
+- (void)setContentView:(JTNavigationView *)aContentView {
+    [contentView removeFromSuperview];
+    contentView = nil;
+    contentView = aContentView;
+    [self addSubview:aContentView];
+}
+
+- (void)setSidebarView:(JTNavigationView *)aSidebarView {
+    [sidebarView removeFromSuperview];
+    sidebarView = nil;
+    sidebarView = aSidebarView;
+    [self insertSubview:sidebarView atIndex:0];
+}
+
+- (BOOL)isSidebarShowing {
+    return _state.isShowing == 1 ? YES : NO;
+}
+
+#pragma mark Instance method
+
+- (void)revealSidebar:(BOOL)shouldReveal {
+    [UIView beginAnimations:@"" context:nil];
+    [UIView setAnimationDuration:0.3];
+    if (shouldReveal) {
+        contentView.frame = CGRectOffset(contentView.bounds, CGRectGetWidth(sidebarView.frame), 0);
+    } else {
+        contentView.frame = contentView.bounds;
+    }
+    [UIView commitAnimations];
+    _state.isShowing = shouldReveal ? 1 : 0;
+}
+
+#pragma mark Class method
+
++ (JTRevealSidebarView *)defaultViewWithFrame:(CGRect)frame {
+    JTRevealSidebarView *revealView = [[JTRevealSidebarView alloc] initWithFrame:frame];
+    revealView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+
+    JTNavigationView *sidebarView = [[JTNavigationView alloc] initWithFrame:CGRectMake(0, 0, 270, CGRectGetHeight(frame))];
+    {
+        [sidebarView setNavigationBarHidden:YES animated:NO];
+        sidebarView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleRightMargin;
+        revealView.sidebarView = sidebarView;
+    }
+    
+    JTNavigationView *contentView = [[JTNavigationView alloc] initWithFrame:(CGRect){CGPointZero, frame.size} animationStyle:JTNavigationViewAnimationStyleCoverUp];
+    {
+        contentView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        contentView.backgroundColor = [UIColor lightGrayColor];
+        revealView.contentView = contentView;
+    }
+
+    return revealView;
+}
+
+@end
