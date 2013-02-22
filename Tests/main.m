a7decde8abd2a3d77a4c5ecdad367ee425763c0c
Steven Sheldon <steven@sasheldon.com>
2013-02-21 20:48:11 -0600

    Added GHUnit Tests target.

---
+++ Tests/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by Steven Sheldon on 2/21/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+  }
+}
