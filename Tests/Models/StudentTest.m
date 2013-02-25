6e39c9ac659fa6b9dc95c53982bd280176079d53
Steven Sheldon <steven@sasheldon.com>
2013-02-24 23:44:49 -0600

    Added student model test.

---
+++ Tests/Models/StudentTest.m
@@ -0,0 +1,55 @@
+//
+//  StudentTest.m
+//  MobileTA
+//
+//  Created by Steven Sheldon on 2/24/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+#import "Student.h"
+
+@interface StudentTest : GHTestCase
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@end
+
+@implementation StudentTest
+
++ (NSManagedObjectContext *)managedObjectContextForModelsInBundle:(NSBundle *)bundle {
+  NSManagedObjectModel *model = [NSManagedObjectModel mergedModelFromBundles:[NSArray arrayWithObject:bundle]];
+  NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+  NSPersistentStore *store = [coordinator addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+  if (!store) {
+    return nil;
+  }
+  NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
+  context.persistentStoreCoordinator = coordinator;
+  return context;
+}
+
+- (void)setUp {
+  self.managedObjectContext = [StudentTest managedObjectContextForModelsInBundle:[NSBundle mainBundle]];
+  if (!self.managedObjectContext) {
+    GHFail(@"Could not create in-memory store.");
+  }
+}
+
+- (void)tearDown {
+  self.managedObjectContext = nil;
+}
+
+- (void)testFetchStudents {
+  Student *student = [Student studentWithFirstName:@"Steven" lastName:@"Sheldon" context:self.managedObjectContext];
+  GHAssertEqualStrings(student.firstName, @"Steven", nil);
+  GHAssertEqualStrings(student.lastName, @"Sheldon", nil);
+  GHAssertTrue([self.managedObjectContext save:nil], @"Could not save managed object changes.");
+
+  NSArray *students = [Student fetchStudentsInContext:self.managedObjectContext];
+  GHAssertEquals(students.count, 1U, nil);
+  Student *savedStudent = [students objectAtIndex:0];
+  GHAssertEqualStrings(savedStudent.firstName, @"Steven", nil);
+  GHAssertEqualStrings(savedStudent.lastName, @"Sheldon", nil);
+}
+
+@end
