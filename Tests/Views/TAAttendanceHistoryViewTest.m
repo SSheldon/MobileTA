7a61ebb28bb6258437a00c78bb3b7d6c665c60b1
Steven Sheldon <steven@sasheldon.com>
2013-03-11 21:44:33 -0500

    Added Attendance history view test.

---
+++ Tests/Views/TAAttendanceHistoryViewTest.m
@@ -0,0 +1,45 @@
+//
+//  TAAttendanceHistoryViewTest.m
+//  MobileTA
+//
+//  Created by Steven Sheldon on 3/11/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+#import "AttendanceRecord.h"
+#import "TAAttendanceHistoryViewController.h"
+
+@interface TAAttendanceHistoryViewTest : GHViewTestCase
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@end
+
+@implementation TAAttendanceHistoryViewTest
+
+- (void)setUp {
+  self.managedObjectContext = [TATestUtils managedObjectContextForModelsInBundle:[NSBundle mainBundle]];
+  if (!self.managedObjectContext) {
+    GHFail(@"Could not create in-memory store.");
+  }
+}
+
+- (void)tearDown {
+  self.managedObjectContext = nil;
+}
+
+- (void)test {
+  TAAttendanceHistoryViewController *controller = [[TAAttendanceHistoryViewController alloc] init];
+  controller.records = @[
+    [AttendanceRecord attendanceRecordForName:@"Discussion Section"
+                                         date:[NSDate dateWithTimeIntervalSince1970:1331467200]
+                                      context:self.managedObjectContext],
+    [AttendanceRecord attendanceRecordForName:@"Exam Review"
+                                         date:[NSDate dateWithTimeIntervalSince1970:1331294400]
+                                      context:self.managedObjectContext]
+  ];
+  [controller.view layoutSubviews];
+  GHVerifyView(controller.view);
+}
+
+@end
