fc771bfdea33d95270cd84d7c1997a4c5bd04ded
Steven Sheldon <steven@sasheldon.com>
2013-04-27 01:52:10 -0500

    Added students groups VC.

---
+++ MobileTA/Classes/Controllers/TAStudentsGroupsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TAStudentsGroupsViewController.h
+//  MobileTA
+//
+//  Created by Steven Sheldon on 4/26/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import "TAStudentsAttendanceViewController.h"
+
+@interface TAStudentsGroupsViewController : TAStudentsAttendanceViewController
+
+@property (strong, nonatomic) NSArray *groups;
+
+@end
---
+++ MobileTA/Classes/Controllers/TAStudentsGroupsViewController.m
@@ -0,0 +1,83 @@
+//
+//  TAStudentsGroupsViewController.m
+//  MobileTA
+//
+//  Created by Steven Sheldon on 4/26/13.
+//  Copyright (c) 2013 Steven Sheldon. All rights reserved.
+//
+
+#import "TAStudentsGroupsViewController.h"
+
+#import "Group.h"
+
+@implementation TAStudentsGroupsViewController
+
+- (void)setGroups:(NSArray *)groups {
+  NSArray *sortDescriptors = @[
+    [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)],
+  ];
+  _groups = [groups sortedArrayUsingDescriptors:sortDescriptors];
+  [self reloadStudents];
+}
+
+#pragma mark TAStudentsViewController
+
+- (NSArray *)students {
+  [self doesNotRecognizeSelector:_cmd];
+  return nil;
+}
+
+- (void)setStudents:(NSArray *)students {
+  [self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)reloadStudents {
+  _tableSections = [[NSMutableArray alloc] init];
+
+  // Add the sorted students for each section
+  NSArray *sortDescriptors = @[
+    [NSSortDescriptor sortDescriptorWithKey:@"lastName" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)],
+    [NSSortDescriptor sortDescriptorWithKey:@"firstName" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)],
+  ];
+  for (Group *group in self.groups) {
+    NSMutableArray *students = [NSMutableArray arrayWithArray:[group.students allObjects]];
+    [students sortUsingDescriptors:sortDescriptors];
+    [_tableSections addObject:students];
+  }
+
+  if ([self isViewLoaded]) {
+    [self.tableView reloadData];
+  }
+}
+
+- (void)removeStudent:(Student *)student {
+  // We don't have a students array, so override this with an empty method
+}
+
+- (NSIndexPath *)indexPathOfStudent:(Student *)student {
+  NSInteger sectionIndex = [self.groups indexOfObject:student.group];
+  if (sectionIndex == NSNotFound) {
+    return nil;
+  }
+  NSInteger rowIndex = [[_tableSections objectAtIndex:sectionIndex] indexOfObject:student];
+  if (rowIndex == NSNotFound) {
+    return nil;
+  }
+  if (detailedStudentIndex && detailedStudentIndex.section == sectionIndex && detailedStudentIndex.row < rowIndex) {
+    rowIndex++;
+  }
+  return [NSIndexPath indexPathForRow:rowIndex inSection:sectionIndex];
+}
+
+#pragma mark UITableViewDataSource
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  return [[self.groups objectAtIndex:section] name];
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+  // Disable the index
+  return nil;
+}
+
+@end
