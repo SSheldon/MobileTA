6548637f0e011eb4445a49a0b4ee8da16423ad8b
Steven Sheldon <steven@sasheldon.com>
2013-04-28 17:49:41 -0500

    Reordered group color choices.

--- MobileTA/Classes/Models/Group.m
+++ MobileTA/Classes/Models/Group.m
@@ -30,18 +30,18 @@
   static NSArray *items = nil;
   if (items == nil) {
     items = @[
-              @[@"Pink", UIColorFromRGB(0xFF1493)],
-              @[@"Sand", UIColorFromRGB(0xFFFF7B)],
-              @[@"Teal", UIColorFromRGB(0x00FF99)],
-              @[@"Purple", UIColorFromRGB(0xA020F0)],
-              @[@"Goldenrod", UIColorFromRGB(0xE3DF00)],
-              @[@"Nighttime", UIColorFromRGB(0x002E6D)],
-              @[@"Orangered", UIColorFromRGB(0xFF4258)],
-              @[@"Fried Rice", UIColorFromRGB(0xB86800)],
-              @[@"Leaf Green", UIColorFromRGB(0x006E1A)],
-              @[@"Orange Tang", UIColorFromRGB(0xFFBD00)],
-              @[@"Cerulean Blue", UIColorFromRGB(0x009BDD)],
-              @[@"Princess Peach", UIColorFromRGB(0xFFBBA8)],
+      @[@"Purple", UIColorFromRGB(0xA020F0)],
+      @[@"Cerulean", UIColorFromRGB(0x009BDD)],
+      @[@"Nighttime", UIColorFromRGB(0x002E6D)],
+      @[@"Leaf Green", UIColorFromRGB(0x006E1A)],
+      @[@"Mint Green", UIColorFromRGB(0x00FF99)],
+      @[@"Sand", UIColorFromRGB(0xFFFF7B)],
+      @[@"Goldenrod", UIColorFromRGB(0xE3DF00)],
+      @[@"Orange Tang", UIColorFromRGB(0xFFBD00)],
+      @[@"Orangered", UIColorFromRGB(0xFF4258)],
+      @[@"Pink", UIColorFromRGB(0xFF1493)],
+      @[@"Princess Peach", UIColorFromRGB(0xFFBBA8)],
+      @[@"Fried Rice", UIColorFromRGB(0xB86800)],
     ];
   }
   return items;
