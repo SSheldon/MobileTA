3f811a4ebe1e2a04e3be99458dd36f2d5243be08
Scott Rice <meris608@gmail.com>
2013-04-28 15:52:32 -0500

    Named all of the colors.
    STEVEN IF YOU CHANGE THESE NAMES I WILL END YOU.

--- MobileTA/Classes/Models/Group.m
+++ MobileTA/Classes/Models/Group.m
@@ -30,26 +30,18 @@
   static NSArray *items = nil;
   if (items == nil) {
     items = @[
-              @[@"Color1", UIColorFromRGB(0xC2C5FF)],
-              @[@"Color2", UIColorFromRGB(0xAA5555)],
-              @[@"Color3", UIColorFromRGB(0xFFBBA8)],
-              @[@"Color4", UIColorFromRGB(0xFFFF7B)],
-              @[@"Color5", UIColorFromRGB(0xFF4258)],
-              @[@"Color6", UIColorFromRGB(0xBF36FF)],
-              @[@"Color7", UIColorFromRGB(0x002E6D)],
-              @[@"Color8", UIColorFromRGB(0x009BDD)],
-              @[@"Color9", UIColorFromRGB(0x006E1A)],
-              @[@"Color10", UIColorFromRGB(0x00A76B)],
-              @[@"Color11", UIColorFromRGB(0xE3DF00)],
-              @[@"Color12", UIColorFromRGB(0xE94A00)],
-              @[@"Color13", UIColorFromRGB(0x00FF11)],
-              @[@"Color14", UIColorFromRGB(0xB86800)],
-              @[@"Color15", UIColorFromRGB(0xFFBD00)],
-              @[@"Color16", UIColorFromRGB(0x4B004F)],
-              @[@"Color17", UIColorFromRGB(0xA200AB)],
-              @[@"Color18", UIColorFromRGB(0xFF0000)],
-              @[@"Color19", UIColorFromRGB(0x547300)],
-              @[@"Color20", UIColorFromRGB(0x00FF99)]
+              @[@"Pink",UIColorFromRGB(0xFADADD)],
+              @[@"Teal", UIColorFromRGB(0x00FF99)],
+              @[@"Purple", UIColorFromRGB(0xA200AB)],
+              @[@"Nighttime", UIColorFromRGB(0x002E6D)],
+              @[@"Orangered", UIColorFromRGB(0xFF4258)],
+              @[@"Fried Rice", UIColorFromRGB(0xB86800)],
+              @[@"Orange Tang", UIColorFromRGB(0xFFBD00)],
+              @[@"Purple Nurple", UIColorFromRGB(0xBF36FF)],
+              @[@"Cerulean Blue", UIColorFromRGB(0x009BDD)],
+              @[@"Princess Peach", UIColorFromRGB(0xFFBBA8)],
+              @[@"Goldenrod City", UIColorFromRGB(0xE3DF00)],
+              @[@"Pokemon Leafgreen", UIColorFromRGB(0x006E1A)],
     ];
   }
   return items;
